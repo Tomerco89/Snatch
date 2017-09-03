3093c529e17aeddccc7654df1b9127d1aad28672
tomercohen <tomerco911@gmail.com>
2017-09-03 20:27:15 +0300

    deploying to github

--- SnatchApp/ItemView.swift
+++ SnatchApp/ItemView.swift
@@ -2,7 +2,7 @@
 //  ItemView.swift
 //  SnatchApp
 //
-//  Created by Tomer Cohen on 18/03/2017.
+//  Created by Tomer Cohen on 25/06/2017.
 //  Copyright © 2017 Tomer Cohen. All rights reserved.
 //
 
@@ -10,11 +10,12 @@ import UIKit
 
 class ItemView: UIView {
 
-    @IBOutlet var photoImageView: UIImageView?
-    @IBOutlet var photoTitleLabel: UILabel?
-    
-    func getView() -> UIView {
-        return Bundle.main.loadNibNamed("ItemView", owner: nil, options: nil)![0] as! UIView
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
     }
+    */
 
 }
